2d3b00cd09bbc340c44181a675b1f74f8d9a3bf1
Filip Navara <filip.navara@gmail.com>
2023-08-09 13:33:27 +0200

    Add command line interface and packing options

--- Program.cs
+++ Program.cs
@@ -1,21 +1,27 @@
 ﻿using Avalonia;
 using System;
+using System.CommandLine;
 
 namespace OneHub.Tools.HeapView;
 
 class Program
 {
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static int Main(string[] args)
+    {
+        var fileNameArgument = new Argument<string>("filename", "Path to .gcdump file");
+        var cmd = new RootCommand { fileNameArgument };
+        cmd.SetHandler(HandleView, fileNameArgument);
+        return cmd.Invoke(args);
+    }
 
-    // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+    static void HandleView(string inputFileName)
+    {
+        AppBuilder
+            .Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
-            .LogToTrace();
+            .LogToTrace()
+            .StartWithClassicDesktopLifetime(new[] { inputFileName });
+    }
 }
